361e0ee473783c2b3e3327a4098ece9669ddeae6
Jorge Vidal Casa <jlvidal_2hotmail.com>
2023-06-10 11:13:01 -0500

    Primer commmit del proyecto, se subern los archivos de la primera etapa, model y view

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+b
+2src/main/java/com/example/demo/model/Producto.java,2\1\212dd9e0cee518235be1a35a4a1f000f1f5ecadf
+c
+3src/main/java/com/example/demo/DemoApplication.java,7\1\7171df7c024808a26040ea210fc78aec82a83023
---
+++ demo/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @GetMapping("")
+    public String Home() {
+
+        return "usuario/home.html";
+    }
+}
